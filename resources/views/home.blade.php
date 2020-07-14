0c9dee8367891b9ff8e1368bbb6fd07087b8fa11
vinayakwebtize <vinayak@webtize.com.au>
2020-07-14 18:48:12 +0530

    init

---
+++ resources/views/home.blade.php
@@ -0,0 +1,80 @@
+<html>
+<head>
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="map"></div>
+            </div>
+        </div>
+
+        <div class="row">
+            <form method="" action="">
+                <div class="col-md-12">
+                    <div class="my-form">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <input type="text" class="form-control" name="street_address" />
+                            </div>
+                            <div class="col-md-3">
+                                <input type="text" class="form-control" name="city" />
+                            </div>
+                            <div class="col-md-2">
+                                <input type="text" class="form-control" name="state" />
+                            </div>
+                            <div class="col-md-2">
+                                <input type="text" class="form-control" name="zipcode" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <div class="row">
+                        <div class="col-md-2">
+                            <div class="property-radio">
+                                <input type="radio" class="form-control" value="1" name="property_type" />
+                            </div>
+
+                        </div>
+                        <div class="col-md-2">
+                            <div class="property-radio">
+                                <input type="radio" class="form-control" value="2" name="property_type" />
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="property-radio">
+                                <input type="radio" class="form-control" value="3" name="property_type" />
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="property-radio">
+                                <input type="radio" class="form-control" value="4" name="property_type" />
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="property-radio">
+                                <input type="radio" class="form-control" value="5" name="property_type" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <button type="submit" class="btn btn-success">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</html>
