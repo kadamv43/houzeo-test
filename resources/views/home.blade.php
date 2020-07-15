77cdfc208df024251d3c1fefe1e7bf2b9f7cf7f8
vinayakwebtize <vinayak@webtize.com.au>
2020-07-15 15:40:22 +0530

    changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -172,7 +172,7 @@
             </div>
         </div>
 
-        <div class="row">
+        <div class="row" style="margin-top: 3%">
             <div class="col-md-12">
                 <div class="card">
                     <div class="card-body">
