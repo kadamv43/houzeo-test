9368b845a34c4a099ea4b3cf2927b66eaf845e43
vinayakwebtize <vinayak@webtize.com.au>
2020-07-18 16:18:08 +0530

    small changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -262,7 +262,6 @@
                 console.log(ui.item.label);
                 console.log(this.value);
                 codeAddress(geocoder, map);
-                //geocodeLatLng(geocoder, map, infowindow);
                 $('[name=city]').val(ui.item.city);
                 $('[name=state]').val(ui.item.state);
                 $('#map').show();
@@ -340,34 +339,6 @@
         });
     }
 
-    function geocodeLatLng(geocoder, map, infowindow) {
-        var input = document.getElementById("latlng").value;
-        var latlngStr = input.split(",", 2);
-        var latlng = {
-            lat: parseFloat(latlngStr[0])
-            , lng: parseFloat(latlngStr[1])
-        };
-        geocoder.geocode({
-            location: latlng
-        }, function(results, status) {
-            console.log(results);
-            if (status === "OK") {
-                if (results[0]) {
-                    map.setZoom(11);
-                    var marker = new google.maps.Marker({
-                        position: latlng
-                        , map: map
-                    });
-                    infowindow.setContent(results[0].formatted_address);
-                    infowindow.open(map, marker);
-                } else {
-                    window.alert("No results found");
-                }
-            } else {
-                window.alert("Geocoder failed due to: " + status);
-            }
-        });
-    }
 
 </script>
 </html>
